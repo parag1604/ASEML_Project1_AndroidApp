b82a370ca7de32777f631848e05f8dbd6a2a3a1d
Parag Dutta <parag1604@hotmail.com>
2021-03-02 17:46:34 +0530

    included voice input functionality

--- app/src/main/java/com/example/myapplicationbyparag/MainActivity.java
+++ app/src/main/java/com/example/myapplicationbyparag/MainActivity.java
@@ -1,31 +1,60 @@
 package com.example.myapplicationbyparag;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.speech.RecognizerIntent;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button send_button;
+    ImageView voice_button;
+    EditText input_text;
+
+    private static final int RECOGNIZER_RESULT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        input_text = (EditText) findViewById(R.id.input_text);
+
         send_button = (Button) findViewById(R.id.send_button);
-        send_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewActivity();
-            }
-        });
+        send_button.setOnClickListener(v -> openNewActivity());
+
+        voice_button = (ImageView) findViewById(R.id.voice_button);
+        Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice to text");
+        voice_button.setOnClickListener(v -> startActivityForResult(speechIntent,
+                                                                    RECOGNIZER_RESULT));
+
     }
+
     public void openNewActivity(){
         Intent intent = new Intent(this, SubActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == RECOGNIZER_RESULT && resultCode == RESULT_OK) {
+            assert data != null;
+            ArrayList<String> recognizedText = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            input_text.setText(recognizedText.get(0));
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/example/myapplicationbyparag/SubActivity.java
+++ app/src/main/java/com/example/myapplicationbyparag/SubActivity.java
@@ -1,41 +1,38 @@
 package com.example.myapplicationbyparag;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import java.util.Objects;
 
 public class SubActivity extends AppCompatActivity {
 
     Button confirm_button;
+    EditText text_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        text_input = (EditText) findViewById(R.id.emailOrNumber);
 
         Context context = getApplicationContext();
-        CharSequence sms_text = "Sending message...";
-        CharSequence email_text = "Sending email...";
-        CharSequence error_text = "Not a valid email or phone number";
         int duration = Toast.LENGTH_SHORT;
+        String toast_text = "Testing...";
 
         confirm_button = (Button) findViewById(R.id.confirm_button);
-        confirm_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayToast(context, error_text, duration);
-            }
-        });
+        confirm_button.setOnClickListener(v -> displayToast(context, toast_text, duration));
 
     }
 
-    public void displayToast(Context context, CharSequence text, int duration){
+    public void displayToast(Context context, String text, int duration){
         Toast.makeText(context, text, duration).show();
     }
 }
